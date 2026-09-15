e63013a1b26dc0c051deaff27b3d961d7e6bd460
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-09-15 15:21:22 +0330

    feat: define users table model, complete docker compose and application.yaml file to read parametric .env configuration data

---
+++ identity-service/src/main/java/ir/aspireapps/identityservice/model/User.java
@@ -0,0 +1,63 @@
+package ir.aspireapps.identityservice.model;
+
+import ir.aspireapps.common.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@SQLDelete(sql = """
+        UPDATE users 
+        SET deleted_at = CURRENT_TIMESTAMP 
+        WHERE id = ?
+        """)
+@SQLRestriction("deleted_at IS NULL")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Column(nullable = false, columnDefinition = "user_role")
+    @Builder.Default
+    private Role role = Role.USER;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @Setter(AccessLevel.NONE)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    @Setter(AccessLevel.NONE)
+    private Instant updatedAt;
+
+    @Column(name = "deleted_at")
+    @Builder.Default
+    private Instant deletedAt = null;
+}
