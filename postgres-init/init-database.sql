96fbf561496faa170d4cad67bfb52daafe025429
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-09-15 16:04:39 +0330

    feat: create init-database.sql to create all databases at startup

---
+++ postgres-init/init-database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE catalog_db;
+CREATE DATABASE identity_db;
+CREATE DATABASE inventory_db;
+CREATE DATABASE notification_db;
+CREATE DATABASE order_db;
+CREATE DATABASE payment_db;
+CREATE DATABASE shipping_db;
