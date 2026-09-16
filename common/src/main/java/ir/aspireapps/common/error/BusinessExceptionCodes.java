d06cc5357860d09e5801d398e58dad8c4a245585
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-09-16 15:32:41 +0330

    feat: update BusinessExceptionCodes

--- common/src/main/java/ir/aspireapps/common/error/BusinessExceptionCodes.java
+++ common/src/main/java/ir/aspireapps/common/error/BusinessExceptionCodes.java
@@ -5,4 +5,11 @@ public final class BusinessExceptionCodes {
 
     public static final String DUPLICATED_ENTITY = "DUPLICATED_ENTITY_VALUE_AT_UNIQUE_FIELD";
     public static final String ENTITY_NOT_FOUND = "ENTITY_NOT_FOUND";
+
+    public static final String METHOD_ARGUMENT_NOT_VALID = "VALIDATION_ERROR_AT_METHOD_ARGUMENTS";
+    public static final String INVALID_REQUEST_BODY = "REQUEST_BODY_IS_INVALIED";
+
+    public static final String UNAUTHORIZED_ERROR = "AUTHORIZATION_FAILED";
+
+    public static final String INTERNAL_SERVER_ERROR = "SOMETHING_WENT_WRONG";
 }
