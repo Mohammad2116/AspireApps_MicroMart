991c31af547e6678b35ff6c499f1878f883a334e
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-09-17 11:26:11 +0330

    feat: add access token ClaimsConstants

---
+++ common/src/main/java/ir/aspireapps/common/utility/jwt/JwtClaimConstants.java
@@ -0,0 +1,9 @@
+package ir.aspireapps.common.utility.jwt;
+
+public class JwtClaimConstants {
+    private JwtClaimConstants() {}
+
+    public static final String USER_ID = "userId";
+    public static final String USER_ROLES = "roles";
+
+}
