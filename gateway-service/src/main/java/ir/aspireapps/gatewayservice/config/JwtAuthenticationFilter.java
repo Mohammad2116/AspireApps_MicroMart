c01e61005c5411a24afe0b03086f11ca01426c7c
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-09-19 17:55:56 +0330

    feat: add auth to identify service endpoint paths

--- gateway-service/src/main/java/ir/aspireapps/gatewayservice/config/JwtAuthenticationFilter.java
+++ gateway-service/src/main/java/ir/aspireapps/gatewayservice/config/JwtAuthenticationFilter.java
@@ -20,20 +20,20 @@ import java.util.List;
 public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
     private final List<String> PUBLIC_PATHS = List.of(
             "/ir/aspireapps/micromart/home",
-            "/ir/aspireapps/micromart/identify/api/v1/register",
-            "/ir/aspireapps/micromart/identify/api/v1/login",
-            "/ir/aspireapps/micromart/identify/api/v1/refresh",
-            "/ir/aspireapps/micromart/identify/web/v1/register",
-            "/ir/aspireapps/micromart/identify/web/v1/login",
-            "/ir/aspireapps/micromart/identify/web/v1/refresh"
+            "/ir/aspireapps/micromart/identify/api/v1/auth/register",
+            "/ir/aspireapps/micromart/identify/api/v1/auth/login",
+            "/ir/aspireapps/micromart/identify/api/v1/auth/refresh",
+            "/ir/aspireapps/micromart/identify/web/v1/auth/register",
+            "/ir/aspireapps/micromart/identify/web/v1/auth/login",
+            "/ir/aspireapps/micromart/identify/web/v1/auth/refresh"
             );
     private final List<String> WEB_PATHS = List.of(
             "/ir/aspireapps/micromart/home",
-            "/ir/aspireapps/micromart/identify/web/v1/register",
-            "/ir/aspireapps/micromart/identify/web/v1/login",
-            "/ir/aspireapps/micromart/identify/web/v1/refresh",
+            "/ir/aspireapps/micromart/identify/web/v1/auth/register",
+            "/ir/aspireapps/micromart/identify/web/v1/auth/login",
+            "/ir/aspireapps/micromart/identify/web/v1/auth/refresh",
 
-            "/ir/aspireapps/micromart/identify/web/v1/profile"
+            "/ir/aspireapps/micromart/identify/web/v1/auth/profile"
             );
 
     @Override
